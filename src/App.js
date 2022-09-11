20a1cfdddc377d1328ed823ec6217ea26a2d53c0
Edema <edemaero@gmail.com>
2022-09-11 23:17:39 +0100

    update

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { NavBar } from "./components";
 import { Contact, Home, Services } from "./pages";
-import "animate.css";
 
 function App() {
   return (
--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import "./styles/global.css";
+import "animate.css";
 import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
